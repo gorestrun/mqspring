ce5a9239373c8d64ebf67023ebf7bd00d4ed80c3
gorestrun <5555867+gorestrun@users.noreply.github.com>
2021-08-04 12:13:31 +0800

    first commit

---
+++ src/main/java/com/example/MqspringApplication.java
@@ -0,0 +1,45 @@
+package com.example;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jms.JmsException;
+import org.springframework.jms.annotation.EnableJms;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@EnableJms
+@RestController
+@SpringBootApplication
+public class MqspringApplication {
+
+	@Autowired
+	private JmsTemplate jmsTemplate;
+	
+	public static void main(String[] args) {
+		SpringApplication.run(MqspringApplication.class, args);
+	}
+	
+	@GetMapping("send")
+	String send() {
+		try {
+    		jmsTemplate.convertAndSend("DEV.QUEUE.1", "Hello World!");
+    		return "OK";
+		} catch (JmsException e) {
+			e.printStackTrace();
+			return "FAIL";
+		}
+	}
+	
+	@GetMapping("recv")
+	String recv(){
+	    try{
+	        return jmsTemplate.receiveAndConvert("DEV.QUEUE.1").toString(); //Blocking
+	    }catch(JmsException ex){
+	        ex.printStackTrace();
+	        return "FAIL";
+	    }
+	}
+
+}
