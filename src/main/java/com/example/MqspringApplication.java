0d418e86300f67dd81eba387dbc9c292f1486cc2
gorestrun <5555867+gorestrun@users.noreply.github.com>
2021-08-10 16:00:41 +0800

    Few changes to make it work on IBM WAS 8.5.5.18

--- src/main/java/com/example/MqspringApplication.java
+++ src/main/java/com/example/MqspringApplication.java
@@ -1,8 +1,17 @@
 package com.example;
 
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.jms.JmsException;
 import org.springframework.jms.annotation.EnableJms;
 import org.springframework.jms.core.JmsTemplate;
@@ -10,36 +19,68 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @EnableJms
-@RestController
 @SpringBootApplication
-public class MqspringApplication {
-
-	@Autowired
-	private JmsTemplate jmsTemplate;
-	
+public class MqspringApplication extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(MqspringApplication.class, args);
 	}
 	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(MqspringApplication.class);
+    }
+}
+
+@RestController
+class Controller{
+	private static Logger log = LoggerFactory.getLogger(Controller.class);
+	
+	@Autowired
+	private JmsTemplate jmsTemplate;
+	
+	@GetMapping(value = "/hi", produces = { MediaType.APPLICATION_JSON_VALUE } )
+	public ResponseEntity<ResponseDto> hi(HttpServletResponse response) {
+		ResponseDto responseDto = new ResponseDto();
+		responseDto.setResponse("hi");
+		
+		return  new ResponseEntity<>(responseDto, HttpStatus.OK); 
+	}
+	
 	@GetMapping("send")
-	String send() {
+	public ResponseEntity<ResponseDto> send() {
+		ResponseDto responseDto = new ResponseDto();
+		
 		try {
-    		jmsTemplate.convertAndSend("DEV.QUEUE.1", "Hello World!");
-    		return "OK";
+    		jmsTemplate.convertAndSend("SC.CASS.TNI_INPPMT", "Hello World!");
+    		responseDto.setResponse("OK");
+    		
+    		return new ResponseEntity<>(responseDto, HttpStatus.OK);
 		} catch (JmsException e) {
 			e.printStackTrace();
-			return "FAIL";
+			log.error("Exception: ", e);
+			responseDto.setResponse("FAIL");
+			
+			return new ResponseEntity<>(responseDto, HttpStatus.OK);
 		}
 	}
 	
-	@GetMapping("recv")
-	String recv(){
+	@GetMapping("receive")
+	public ResponseEntity<ResponseDto> recv(){
+		ResponseDto responseDto = new ResponseDto();
+		
 	    try{
-	        return jmsTemplate.receiveAndConvert("DEV.QUEUE.1").toString(); //Blocking
+	    	Object message = jmsTemplate.receiveAndConvert("SC.CASS.TNI_INPPMT"); // Blocking
+	    	if(message != null) {
+	    		log.info("Message received: {}", message);
+	    	}
+	    	responseDto.setResponse("OK");
+	    	
+	        return new ResponseEntity<>(responseDto, HttpStatus.OK); 
 	    }catch(JmsException ex){
 	        ex.printStackTrace();
-	        return "FAIL";
+	        responseDto.setResponse("FAIL");
+	        
+	        return new ResponseEntity<>(responseDto, HttpStatus.OK);
 	    }
 	}
-
 }
---
+++ src/main/java/com/example/ResponseDto.java
@@ -0,0 +1,44 @@
+package com.example;
+
+public class ResponseDto {
+
+	private String response;
+
+	public String getResponse() {
+		return response;
+	}
+
+	public void setResponse(String response) {
+		this.response = response;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((response == null) ? 0 : response.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ResponseDto other = (ResponseDto) obj;
+		if (response == null) {
+			if (other.response != null)
+				return false;
+		} else if (!response.equals(other.response))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "ResponseDto [response=" + response + "]";
+	}
+}
